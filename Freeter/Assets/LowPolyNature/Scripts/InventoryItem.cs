4bad056712938ed43b0342fa2330f24ab66d6d4c
ZeroOneTheory <36996656+ZeroOneTheory@users.noreply.github.com>
2018-03-02 17:02:31 -0500

    First Level ready for edits
    added some custom Assets in to test out some additional game mechanics

---
+++ Freeter/Assets/LowPolyNature/Scripts/InventoryItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInventoryItem {
+
+    string Name { get; }
+
+    Sprite Image { get;  }
+
+    void OnPickup();
+
+    void OnDrop();
+
+    void OnUse();
+
+    InventorySlot Slot { get; set; }
+}
+
+public class InventorySlotEventArgs : EventArgs
+{
+    public InventorySlotEventArgs(InventorySlot slot)
+    {
+        Slot = slot;
+    }
+
+    public InventorySlot Slot;
+}
+
+public class InventoryEventArgs : EventArgs
+{
+    public InventoryEventArgs(IInventoryItem item)
+    {
+        Item = item;
+    }
+
+    public IInventoryItem Item;
+}
+
