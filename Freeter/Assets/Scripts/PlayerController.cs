2ce174f0514bd31c0ae8206b3d2e48f5847793fa
ZeroOneTheory <36996656+ZeroOneTheory@users.noreply.github.com>
2018-03-02 22:47:06 -0500

    Bam! Camera!
    Camera works now too, use A & D to rotate around, mouse wheel to zoom, and click to destination

--- Freeter/Assets/Scripts/PlayerController.cs
+++ Freeter/Assets/Scripts/PlayerController.cs
@@ -21,19 +21,29 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)) //--------------- Movement
         {
             Ray ray = Cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
             if(Physics.Raycast(ray,out hit, 100, movementMask))
             {
-                // Move our player to what we hit
-                Debug.Log("We hit " + hit.collider.name + hit.point);
-                motor.MoveToPoint(hit.point);
-
+             // Move our player to what we hit
+             motor.MoveToPoint(hit.point);
                 // Stop Focusing any Objects
             }
         }
-	}
+
+        if (Input.GetMouseButtonDown(1)) // ------------ Interactions
+        {
+            Ray ray = Cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                // Check if we hit interactable 
+                // if we did set it as our focus
+            }
+        }
+    }
 }
