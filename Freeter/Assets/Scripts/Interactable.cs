c7260aecb7b9ce41f7cbb61ac25010a51b7e661d
ZeroOneTheory <36996656+ZeroOneTheory@users.noreply.github.com>
2018-03-03 00:23:32 -0500

    Interactables Done!
    Interactables Done! Added a key ready with virtual interaction functions ready for overwride.

--- Freeter/Assets/Scripts/Interactable.cs
+++ Freeter/Assets/Scripts/Interactable.cs
@@ -3,10 +3,47 @@
 public class Interactable : MonoBehaviour {
 
     public float radius = 3f;
+    public Transform interactionTransform;
+    public bool isFocused = false;
+    bool hasInteracted = false;
 
-   void OnDrawGizmosSelected()
+    public virtual void Interact()
+    {
+        // this method is meant to be overwritten
+        Debug.Log("Interacting with Object " + transform.name);
+    }
+
+    Transform Player;
+
+    void Update()
+    {
+        if (isFocused && !hasInteracted)
+        {
+            float Distance = Vector3.Distance(Player.position, interactionTransform.position);
+            if(Distance <= radius)
+            {
+                hasInteracted = true;
+                Interact();
+            }
+        }
+    }
+
+    public void OnFocus(Transform playersTransform)
+    {
+        isFocused = true;
+        hasInteracted = false;
+        Player = playersTransform;
+    }
+    public void OnDeFocused()
+    {
+        isFocused = false;
+        hasInteracted = false;
+        Player = null;
+    }
+
+    void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, radius);
+        Gizmos.DrawWireSphere(interactionTransform.position, radius);
     }
 }
--- Freeter/Assets/Scripts/PlayerController.cs
+++ Freeter/Assets/Scripts/PlayerController.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/* Controls the players, by using the mouse to set a focus and moving towards that position */
+
+
 [RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : MonoBehaviour {
 
-    public LayerMask movementMask;
+    public LayerMask movementMask; // Filter out everything that isnt walkable
+    public Interactable Focus; // Players Current Focus
 
-    Camera Cam;
-    PlayerMotor motor;
+    Camera Cam; // refrence to ous Camera 
+    PlayerMotor motor;  // Refrence to our Motor 
 
 
-	// Use this for initialization
+	// Getting Refrences
 	void Start () {
         Cam = Camera.main;
         motor = GetComponent<PlayerMotor>();
@@ -21,29 +26,64 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        // if Left mouse is pressed
         if (Input.GetMouseButtonDown(0)) //--------------- Movement
         {
+
+            //Create Ray 
             Ray ray = Cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            if(Physics.Raycast(ray,out hit, 100, movementMask))
+            //If the Ray hits
+            if (Physics.Raycast(ray,out hit, 100, movementMask))
             {
              // Move our player to what we hit
              motor.MoveToPoint(hit.point);
-                // Stop Focusing any Objects
+
+             RemoveFocus(); // Unfocus on object(s)
             }
         }
-
+        // if Right mouse is pressed
         if (Input.GetMouseButtonDown(1)) // ------------ Interactions
         {
+            //Create Ray 
             Ray ray = Cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
+            //If the Ray hits
             if (Physics.Raycast(ray, out hit, 100))
             {
-                // Check if we hit interactable 
-                // if we did set it as our focus
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                if (interactable != null)
+                {
+                    SetFocus(interactable);
+                }
             }
         }
+    }
+
+    void SetFocus(Interactable newFocus)
+    {
+        if(newFocus!= Focus)
+        {
+            if(Focus!=null)
+                Focus.OnDeFocused();
+
+            Focus = newFocus;
+            motor.FollowTarget(newFocus);
+        }
+
+        newFocus.OnFocus(transform);
+        
+
+    }
+    void RemoveFocus()
+    {
+        if (Focus != null)
+            Focus.OnDeFocused();
+
+        Focus = null;
+        motor.StopFollowingTarget();
     }
 }
