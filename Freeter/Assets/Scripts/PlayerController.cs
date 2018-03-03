9d70286d35f4904dabb3038a7b7cc356c8cceeac
ZeroOneTheory <36996656+ZeroOneTheory@users.noreply.github.com>
2018-03-02 20:33:09 -0500

    Latest Update
    just catching up the files

---
+++ Freeter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private CapsuleCollider Col;
+    private Animation Anim;
+
+    public Rigidbody rb;
+    public CharacterController Char;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
