05e9019f3771b55198dae5002849a6ca33d0912b
ZeroOneTheory <36996656+ZeroOneTheory@users.noreply.github.com>
2018-03-04 15:37:12 -0500

    my changes

---
+++ Freeter/Assets/InventoryUI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class InventoryUI : MonoBehaviour {
+
+    Inventory inventory;
+
+	// Use this for initialization
+	void Start () {
+        inventory = Inventory.instance;
+        //inventory.onITemChangedCallBack += UpdateUI();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void UpdateUI()
+    {
+        Debug.Log("Updating UI");
+    }
+}
