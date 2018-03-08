f277e24b71b107a7c4032370a00c3587b0353616
ZeroOneTheory <36996656+ZeroOneTheory@users.noreply.github.com>
2018-03-07 19:15:15 -0500

    random updates

--- Freeter/Assets/Scripts/Items/Equipment.cs
+++ Freeter/Assets/Scripts/Items/Equipment.cs
@@ -2,16 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RollStart : MonoBehaviour {
-
-    public Rigidbody rb;
+public class Equipment : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
-        rb = GetComponent<Rigidbody>();
-
-
+		
 	}
 	
 	// Update is called once per frame
